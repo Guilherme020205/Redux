54efe2e4e7995c777b2cea427427bee3d60314fc
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2026-04-22 20:09:01 -0300

    Basico redux

--- 1/index.js
+++ 1/index.js
@@ -7,7 +7,7 @@ const ADD_TITLE = "ADD_TITLE";
 const ADD_TASK = "ADD_TASK";
 const REMOVE_TASK = "REMOVE_TASK";
 
-// Função que cria a ação => Action Creator
+// Action Creator => Função que cria a ação 
 function addTitle(title){
     return { type: ADD_TITLE, payload: title } // Actions: objeto => tipo da ação e payload ---------------
 }
