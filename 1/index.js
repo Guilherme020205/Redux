c26af574bdd90afdf24c9ebd3acf48d6ac999054
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2026-04-27 19:46:11 -0300

    Update index.js

--- 1/index.js
+++ 1/index.js
@@ -1,6 +1,8 @@
+const redux = require("redux")
+
 // State ---------------
 const initialStateTitle = ""
-const initialStateTask = []
+const initialStateTasks = []
 
 // Actions types ---------------
 const ADD_TITLE = "ADD_TITLE";
@@ -8,14 +10,59 @@ const ADD_TASK = "ADD_TASK";
 const REMOVE_TASK = "REMOVE_TASK";
 
 // Action Creator => Função que cria a ação 
-function addTitle(title){
+function addTitle( title ){
     return { type: ADD_TITLE, payload: title } // Actions: objeto => tipo da ação e payload ---------------
 }
 
-function addTask(task){
+function addTask( task ){
     return { type:ADD_TASK, payload: task } 
 }
 
-function removeTask(task){
+function removeTask( task ){
     return { type:REMOVE_TASK, payload: task}
 }
+
+// Reducers ---------------
+// Recebe o estado atual e a action e retorna como deve ser o novo estado
+// Reducer NÃO altera o estado diretamente, o estado é imutável
+const reducerTitle = ( state = initialStateTitle, action ) => {
+     switch (action.type) {
+        case ADD_TITLE: 
+            return action.payload
+        default:
+            return state
+     } 
+}
+
+const reducerTask = ( state = initialStateTasks, action ) => {
+    switch (action.type) {
+        case ADD_TASK: 
+            return [...state, action.payload]
+        case REMOVE_TASK:
+            return state.filter((t) => t !== action.payload)
+        default:
+            return state
+    }
+}
+
+const rootReducer = redux.combineReducers({
+    title: reducerTitle,
+    tasks: reducerTask,
+})
+
+// Store ---------------
+const store = redux.createStore(rootReducer)
+
+console.log("Inital state", store.getState())
+
+const unsubscribe = store.subscribe(() => console.log("Updated State", store.getState())) // Vai observar cada mudança feita
+
+store.dispatch(addTitle("Lista de tarefas do dia 25/05"))
+store.dispatch(addTask("Comprar as coisas do almoço!"))
+store.dispatch(addTask("Cozinhar almoço"))
+store.dispatch(addTask("Comer o almoço :)"))
+store.dispatch(removeTask("Comprar as coisas do almoço!"))
+
+// console.log("Final state", store.getState())
+
+unsubscribe()
